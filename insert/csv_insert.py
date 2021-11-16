100a6130db0307995efa1e3643b60917f4e48c01
nyh <dkzndk1100@naver.com>
2021-11-16 16:29:04 +0900

    도서목록 데이터베이스 생성하기 book.sql - 도서목록 데이터베이스 구조 csv_insert.py - csv파일 데이터베이스에 commit elice_library.csv - 데이터 정보가 들어 있는 csv파일

---
+++ insert/csv_insert.py
@@ -0,0 +1,31 @@
+import csv
+import pymysql
+import datetime as dt
+from dotenv import load_dotenv
+import os
+
+pw = os.environ.get("PW")
+conn = pymysql.connect(host='localhost', user='root', password=pw,
+                       db='elice_library', charset='utf8')
+
+curs = conn.cursor()
+conn.commit()
+
+with open('elice_library.csv','r') as f:
+    csvReader = csv.reader(f)
+    next(csvReader)
+    for row in csvReader:
+        book_name = row[1]
+        publisher = row[2]
+        author = row[3]
+        publication_date = dt.datetime.strptime(row[4],"%Y-%m-%d")
+        pages = row[5]
+        isbn = row[6]
+        description = row[7]
+        link = row[8]
+        sql = 'INSERT INTO book(book_name,publisher,author,publication_date,pages,isbn,description,link) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)'
+        curs.execute(sql,(book_name,publisher,author,publication_date,pages,isbn,description,link))
+        
+conn.commit()
+f.close()
+conn.close()
