b2a69539e9820114301c98fd10df776b3ce86b3d
nyh <dkzndk1100@naver.com>
2021-11-19 00:07:53 +0900

    대여 테이블 외부키 추가.

--- sql/rent.sql
+++ sql/rent.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `rent` (
   `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `book_id` int NOT NULL,
+  `book_info_id` int NOT NULL,
   `user_id` varchar(20) NOT NULL,
   `rent_date` datetime NOT NULL ,
   `due_date` datetime NOT NULL ,
@@ -8,6 +9,8 @@ CREATE TABLE `rent` (
   `book_return` boolean NOT NULL 
 );
 
-ALTER TABLE `rent` ADD FOREIGN KEY (`book_id`) REFERENCES `book` (`id`);
+ALTER TABLE `rent` ADD FOREIGN KEY (`book_id`) REFERENCES `inventory` (`id`);
+ALTER TABLE `rent` ADD FOREIGN KEY (`book_info_id`) REFERENCES `book` (`id`);
+
 ALTER TABLE `rent` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
