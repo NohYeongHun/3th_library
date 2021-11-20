ddd378613638d1d44b7f59f0a90ff001804283a8
nyh <dkzndk1100@naver.com>
2021-11-21 01:07:59 +0900

    책 평가 내용 출력.

--- views/comment_view.py
+++ views/comment_view.py
@@ -11,24 +11,21 @@ bp = Blueprint('comment', __name__, url_prefix='/comment')
 @bp.route('/<int:id>')
 def home(id):
     
-    # 책의 id값의 정보들
+    # id값에 맞는 책의 정보들
     book_info = rabbitBook.query.filter(rabbitBook.id == id).first()
   
     # 평가가 없을수도 있음.
-    comment_list = []
     try:
         # 리스트로 반환됨.
+        user_name = []
         comments = rabbitComment.query.filter(rabbitComment.book_id == id).all()
-        user_name = rabbitUser.query.filter(rabbitUser.id == comments.user_id).all()
+        for comment in comments:
+            user_name.append(rabbitUser.query.filter(rabbitUser.id == comment.user_id).first().user_name)
+            
     except:
         flash("평가가 없습니다.")
         comments = None
         user_name = None
-
-    
-    print(user_name)
-    
-    
 
     return render_template(
         'comment.html', 
@@ -39,29 +36,23 @@ def home(id):
 
 @bp.route('/submit/<int:id>',methods=["POST"])
 def submit(id):
-
     try:
         comment = request.form['comment']
         rating = request.form['rate']
+        user_id = session['user_id']
+
+        book_id = id
+
+        postComment = rabbitComment(
+            user_id= user_id, 
+            book_id= book_id, 
+            comment= comment, 
+            rating = rating
+        )
+        db.session.add(postComment)
+        db.session.commit()
     except:
         flash("제출하시려면 평점과 댓글을 모두 작성해 주세요.")
 
-    user_id = session['user_id']
-
-    book_id = id
-
-    postComment = rabbitComment(
-        user_id= user_id, 
-        book_id= book_id, 
-        comment= comment, 
-        rating = rating
-    )
-    db.session.add(postComment)
-    db.session.commit()
-
-    #DB write
-    # post = rabbitComment(rating, comment)
-    # db.session.add(post)
-    # db.session.commit()
-
+    
     return redirect(url_for('comment.home',id=id))
