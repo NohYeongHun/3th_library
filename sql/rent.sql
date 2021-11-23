4f90c19a94a29d20522cc63068e5915258595a61
nyh <dkzndk1100@naver.com>
2021-11-24 00:25:46 +0900

    대여 테이블 trigger 추가 대여 기간 = 현재시간 반납 기간 = 현재시간 + 14

--- sql/rent.sql
+++ sql/rent.sql
@@ -3,8 +3,8 @@ CREATE TABLE `rent` (
   `book_id` int NOT NULL,
   `book_info_id` int NOT NULL,
   `user_id` varchar(20) NOT NULL,
-  `rent_date` date  NOT NULL DEFAULT(CURRENT_DATE),
-  `due_date` date NOT NULL DEFAULT(CURRENT_DATE+7),
+  `rent_date` date  NOT NULL,
+  `due_date` date NOT NULL,
   `return_date` datetime,
   `book_return` boolean NOT NULL DEFAULT FALSE
 );
@@ -14,3 +14,8 @@ ALTER TABLE `rent` ADD FOREIGN KEY (`book_info_id`) REFERENCES `book` (`id`);
 
 ALTER TABLE `rent` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
+-- Date Trigger
+CREATE TRIGGER test_trigger BEFORE INSERT ON `rent` 
+FOR EACH ROW SET
+    NEW.rent_date = IFNULL(NEW.rent_date, NOW()),
+    NEW.due_date = TIMESTAMPADD(DAY, 14, NEW.rent_date);
