7cc5a39c0debde12da92486b8d382fb5bd91c3cd
nyh <dkzndk1100@naver.com>
2021-11-25 19:42:24 +0900

    반납, 대여 처리

--- views/rent_view.py
+++ views/rent_view.py
@@ -2,19 +2,21 @@ from operator import ge
 from flask import Blueprint, render_template, request, url_for, session, redirect, flash
 from models.models import *
 from function.calc import avg_calc
+import datetime
 
 bp = Blueprint('rent', __name__, url_prefix='/rent')
 
 @bp.route('/')
 def home():
 
-    # 현재 접속한 사용자의 대여 정보를 가져온다.
+    # 현재 접속한 사용자의 대여 정보를 가져온다. 
     rent_list = rabbitRent.query\
         .join(rabbitBook, rabbitRent.book_info_id == rabbitBook.id)\
         .add_columns(
             rabbitRent.rent_date, rabbitRent.due_date, rabbitRent.book_return, rabbitRent.book_id,
             rabbitBook.book_name, rabbitBook.id
-        ).filter(rabbitRent.user_id == session['user_id'])
+        ).filter(rabbitRent.user_id == session['user_id'])\
+        .filter(rabbitRent.book_return ==False)
     
     rating_list = {} # 레이팅 점수
     book_list = rabbitBook.query.order_by(rabbitBook.id.asc())
@@ -38,13 +40,23 @@ def home():
 def book_return(book_id):
 
     # 책을 반납.
+    now = datetime.datetime.now()
+    nowDate = now.strftime('%Y-%m-%d')
     try:
         inventory = rabbitInventory.query.filter((rabbitInventory.id == book_id) & (rabbitInventory.exist_check==False)).first()
         inventory.query.filter(rabbitInventory.id == book_id).update({"exist_check":(True)})
-        rabbitRent.query.filter(rabbitRent.book_id == book_id).delete()
+        rabbitRent.query.filter(rabbitRent.book_id == book_id).update({"book_return":(True)})
+        rabbitRent.query.filter(rabbitRent.book_id == book_id).update({"return_date":(nowDate)})
         db.session.commit()
         flash("책이 정상적으로 반납되었습니다.")
     except:
         flash("반납 과정에서 오류가 있었습니다.")
 
     return redirect(url_for("rent.home"))
+
+
+@bp.route('/rent_record/<string:user_id>')
+def book_record(user_id):
+    rent_record_list = rabbitRent.query.filter(rabbitRent.user_id == user_id).all()
+    
+    return render_template('rent_record.html',rent_record_list = rent_record_list)
