b4702565db9acd31778881992d12a85217a31faa
nyh <dkzndk1100@naver.com>
2021-11-26 14:14:43 +0900

    코드 정리

--- sql/inventory.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `inventory` (
-  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `book_id` int NOT NULL ,
-  `exist_check` boolean NOT NULL DEFAULT 1
-);
-
-ALTER TABLE `inventory` ADD FOREIGN KEY (`book_id`) REFERENCES `book` (`id`);
--- sql/user.sql
+++
@@ -1,6 +0,0 @@
-create table user(
-    id varchar(20) NOT NULL PRIMARY KEY,
-    pw varchar(255) NOT NULL,
-    user_name varchar(200) NOT NULL,
-    telephone varchar(20)
-);
